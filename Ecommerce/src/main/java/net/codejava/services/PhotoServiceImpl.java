f87328b49bd022e3dbc579cb13dbc16ae27d58a7
= <=tnminh1997123@gmail.com>
2021-01-10 14:43:37 +0700

    Thêm giải thích pull

--- Ecommerce/src/main/java/net/codejava/services/PhotoServiceImpl.java
+++ Ecommerce/src/main/java/net/codejava/services/PhotoServiceImpl.java
@@ -25,6 +25,7 @@ public class PhotoServiceImpl implements PhotoService{
 		photoRepository.deleteById(id);
         System.out.println("minh dep trai so 2");
         System.out.println("ai dam nhan minh 1");
+        System.out.println("pull: Lấy nội dung của remote repository");
 	}
 
 	@Override
